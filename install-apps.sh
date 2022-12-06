13969ec463b638aa4b7b3fa9aba18f827e545954
Chris Shiplet <chris@nearengine.com>
2022-12-06 14:35:23 -0700

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Homebrew
-yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Homebrew (also installs xcode CLI tools)
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Installers/Casks
 brew install mas
 brew tap homebrew/cask-drivers
+brew tap homebrew/cask-versions
 
 # General CLI Tools
-xcode-select --install
 sudo gem install cocoapods
 brew install git
 brew install node
+brew install yarn
 brew install zsh
 brew install zsh-completions
 brew install vim
@@ -19,51 +20,53 @@ brew install tmux
 brew install rustup
 rustup-init -y --default-toolchain nightly
 brew install awscli
+brew install fistbump
 brew cask install ngrok
 
 # React Native
-npm i -g react-native
 brew install watchman
-brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk8
+brew cask install zulu11
 
 # Apps
 brew cask install adobe-creative-cloud
 brew cask install 1password
-brew cask install alfred
 brew cask install android-studio
 brew cask install backblaze
 brew cask install bartender
+mas install 1518425043 # Boop
 brew cask install brave-browser
 brew cask install calibre
+brew cask install calendar-366
 brew cask install charles
+brew cask install daisydisk
 mas install 924726344 # Deliveries
 brew cask install docker
 brew cask install electrum
 brew cask install firefox
+brew cask install flipper
 mas install 982710545 # Forecast Bar
 brew cask install google-chrome
-brew cask install gqrx
 brew cask install istat-menus
 brew cask install iterm2
-brew cask install keybase
 brew cask install logitech-options
+mas install 1568924476 # Mela
 brew cask install microsoft-office
-brew cask install notion
+brew cask install moom
+brew cask install obsidian
 brew cask install paw
-brew cask install postico
-brew cask install sequel-pro
+brew cask install prusaslicer
+brew cask install raycast
 brew cask install sketch
-brew cask install skype
 brew cask install slack
-brew cask install spectacle
 brew cask install spotify
-mas install 585829637 # Todoist
+brew cask install tableplus
 brew cask install transmit
+mas install 1450874784 # Transporter
 brew cask install ubiquiti-unifi-controller
+brew cask install webstorm
 brew cask install virtualbox
-brew cask install visual-studio-code
 brew cask install vlc
+brew cask install xcodes
 mas install 497799835 # Xcode
 sudo xcode-select -s /Applications/Xcode.app
 sudo xcodebuild -license
